3343a979d1a6ab2e45576db16387ff0de91f3ac8
Antony Kalantzis <sdi2100046@di.uoa.gr>
2022-05-31 14:51:35 +0300

    added 1.3

--- 1/q1_3/1_3_a.s
+++ 1/q1_3/1_3_a.s
@@ -5,7 +5,14 @@
 #################################################
 
 	.data
+    .align 0
 endl: 					.asciiz 	"\n"
+byte1:   .byte 0x6677, 0x05, 0x04, 0x03, 0x02, 0x01
+byte2:   .byte 0x84, 0x83, 0x82, 0x81
+byte3:   .byte 0x05
+half:    .half 0x6677
+word1:   .word 0x12345678
+word2:   .word 0x87654321
 
 #################################################
 #												#
@@ -17,8 +24,10 @@ endl: 					.asciiz 	"\n"
 	.globl __start	
 												#read first int and move it to a temp reg
 __start:
+    
+    ulw $t0, word1
 
 
-
+    j Exit 
 Exit:				li 		$v0, 10
 					syscall				#au revoir...
--- 1/q1_3/README.md
+++ 1/q1_3/README.md
@@ -1,1 +1,2 @@
 # question 1.3
+aligning 
